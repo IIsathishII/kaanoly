f744f26c40b9356a384f418d96cad27ce18d4a4e
Sathish Kumar S <sathishkumar.sr@zohocorp.com>
2020-05-31 01:25:54 +0530

    First commit

---
+++ Kaanoly/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Kaanoly
+//
+//  Created by SathishKumar on 31/05/20.
+//  Copyright © 2020 Ghost. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Create the SwiftUI view that provides the window contents.
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
